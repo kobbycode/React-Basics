1a34d7423c833168dcff8201c2ff304fd1a342e9
Alex Ofori <alexco.ao@gmail.com>
2021-10-25 06:33:51 +0000

    Returning Function in jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   function formatUsername(userName) {
     return userName.toUpperCase();
   }
-  const name = 'Alex Ofori';
-  const output = <h1>Hello {formatUsername(name)} !!!!</h1>;
-
-  return output;
+  function Greetings(userName) {
+    if (userName) {
+      return <h1>Hello {formatUsername(userName)} !</h1>;
+    } else {
+      return <h2>Hi This is a wrong user!</h2>;
+    }
+  }
+  return Greetings('Samuel Danso');
 }
 
 export default App;
