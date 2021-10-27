f1108269733834f622114406e896f476997a67b9
Alex Ofori <alexco.ao@gmail.com>
2021-10-27 12:11:44 +0000

    Creating a Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import './App.css';
+import Greetings from './Greetings';
 
 function App() {
-  function formatUsername(userName) {
-    return userName.toUpperCase();
-  }
-  function Greetings(userName) {
-    if (userName) {
-      return <h1>Hello {formatUsername(userName)} !</h1>;
-    } else {
-      return <h2>Hi This is a wrong user!</h2>;
-    }
-  }
-  const profilePicture = '/logo512.png';
-  const image = <img src={profilePicture} alt="logo" />;
-
-  const name = 'Alex Ofori';
-  const outputEl = (
-    <div>
-      <h1>Hello {name}</h1>
-      <p>I am glad to have you !</p>
-      {image}
-    </div>
-  );
-  return outputEl;
+  return <Greetings />;
 }
 
 export default App;
