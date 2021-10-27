eb63d9bc1c7ac13a2139a7c6d4e0828263d3eb37
Alex Ofori <alexco.ao@gmail.com>
2021-10-27 15:41:40 +0000

    Class base component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import Greetings from './Greetings';
+import Writing from './Writing';
 
 function App() {
-  return <Greetings name="Kofi" />;
+  return (<Greetings name="Kofi" />), (<Writing writing="React class" />);
 }
 
 export default App;
