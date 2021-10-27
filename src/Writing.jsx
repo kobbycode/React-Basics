079dd718ec444651e0a28f0a72a79b6eb0c4de60
Alex Ofori <alexco.ao@gmail.com>
2021-10-27 15:42:28 +0000

    Class base component

---
+++ src/Writing.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Writing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Writing: {this.props.writing}</h1>
+      </div>
+    );
+  }
+}
